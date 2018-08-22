81ef691eed5ae4bdf50071a5d27a78865dd08585
zyc13 <zyc7777@gmail.com>
2018-08-23 01:19:03 +0800

    优先级队列demo

--- src/Main.java
+++ src/Main.java
@@ -16,9 +16,5 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        try (Scanner in = new Scanner(new FileInputStream(""), "UTF-8")) {
-            while (in.hasNext())
-                System.out.println(in.next());
-        }
     }
 }
